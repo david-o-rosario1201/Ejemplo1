ba87b38d53423d504efc72cbd47432b53531e0b1
david-o-rosario1201 <finnnn023@gmail.com>
2024-04-17 01:05:17 -0400

    Inyección del Contexto en el program

--- Ejemplo1/Program.cs
+++ Ejemplo1/Program.cs
@@ -1,10 +1,15 @@
 using Ejemplo1.Components;
+using Ejemplo1.DAL;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
 	.AddInteractiveServerComponents();
+
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlite(ConStr));
 
 var app = builder.Build();
 
