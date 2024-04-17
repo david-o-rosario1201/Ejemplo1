35f7c4fe805a61c4aba732f0fff43ca76d576ecb
david-o-rosario1201 <finnnn023@gmail.com>
2024-04-17 00:52:51 -0400

    Creación de la carpeta DAL Y el Contexto.

---
+++ Ejemplo1/DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Ejemplo1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ejemplo1.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Estudiantes> Estudiantes { get; set; }
+}
