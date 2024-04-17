eb2f9052a96b23f93dff3152b0e937612ffaabfc
david-o-rosario1201 <finnnn023@gmail.com>
2024-04-17 00:22:16 -0400

    Creación del modelo Estudiantes.

---
+++ Ejemplo1/Models/Estudiantes.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ejemplo1.Models;
+
+public class Estudiantes
+{
+    [Key]
+    public int EstudianteId { get; set; }
+
+    [Required(ErrorMessage = "Debe ingresar un número de matricula")]
+    [RegularExpression(@"\d{4}-\d{4}", ErrorMessage = "El formato de la matricula debe ser xxxx-xxxx")]
+    public int Matricula { get; set; }
+
+    [Required(ErrorMessage = "Debe ingresar un nombre")]
+    public string Nombre { get; set; }
+
+    [Required(ErrorMessage = "Debe ingresar un apellido")]
+    public string Apellido { get; set; }
+
+    [Required(ErrorMessage = "Debe ingresar una carrera")]
+    public string Carrera { get; set; }
+
+    [Range(17,45, ErrorMessage = "La edad debe ser mayor a 16 y menor a 46")]
+    public int Edad { get; set; }
+}
